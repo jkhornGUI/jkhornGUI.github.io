27a85f50eebce2b1998cb3ccc3512bc752d8fd90
Jerra Khorn <jerra_khorn@student.uml.edu>
2016-05-19 17:03:58 -0400

    rearranging js functions

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/**
+* File: main.js
+* Auhtor: Jerra Khorn, jkhorn1@gmail.com
+* Created: May 19, 2016	5:00PM
+***/
+
+document.ready(
+	smoothScroll();
+);
--- js/smoothscrolling.js
+++ js/smoothscrolling.js
@@ -1,8 +1,15 @@
+/**
+* File: smoothscrolling.js
+* Auhtor: Jerra Khorn, jkhorn1@gmail.com
+* Created: May 19, 2016	5:00PM
+***/
+
 /* 
  * Use jQuery to add smooth scrolling
  * Reference: http://www.w3schools.com/bootstrap/bootstrap_theme_band.asp#band
  */
 
+ /*
 $(document).ready(function(){
   // Add smooth scrolling to all links in navbar + footer link
   $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
@@ -24,3 +31,31 @@ $(document).ready(function(){
     });
   });
 });
+*/
+
+/* 
+ * Use jQuery to add smooth scrolling
+ * Reference: http://www.w3schools.com/bootstrap/bootstrap_theme_band.asp#band
+ */
+
+var smoothScroll = function() {
+	// Add smooth scrolling to all links in navbar + footer link
+	$(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+
+	// Prevent default anchor click behavior
+	event.preventDefault();
+
+	// Store hash
+	var hash = this.hash;
+
+	// Using jQuery's animate() method to add smooth page scroll
+	// The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+	$('html, body').animate({
+		scrollTop: $(hash).offset().top
+	}, 600, function(){
+
+		// Add hash (#) to URL when done scrolling (default click behavior)
+		window.location.hash = hash;
+		});
+	};
+};
